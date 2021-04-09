88fdb697d1632f6709ee13a575d91d43d214347d
Guest <caleb.thompson.17@cnu.edu>
2021-04-09 17:32:04 -0400

    Starting writing main file, but I am running into a few threading issues

--- src/410_proj_thread_log.cpp
+++ src/410_proj_thread_log.cpp
@@ -8,14 +8,18 @@
 #include <thread>
 #include <iostream>
 #include <vector>
+#include <string>
 #include "../includes/constants.h"
 #include "../includes/Logger.h"
+#include <pthread.h>
 
 using namespace std;
 
 //NOTE: you should have no mutexes in this file
 //TODO linker errors?  Did you include the pthread library?   And set the proper dialect?
 //TODO declare globals
+vector<thread> vec;
+bool doWork = true;
 
 /***
  * TODO log info to both file and console. You can do this with 2 Logger objects. 
@@ -26,19 +30,41 @@ using namespace std;
  * returns void
  */
 void fun(string info){
-
+	while (doWork) {
+		cout << "info:\t" << endl;
+	}
 }
+
 int main() {
 	
 	//TODO start as many threads as you have cores (see std::thread::hardware_concurrency())
+	unsigned int coreNum = thread::hardware_concurrency();
+
 	//TODO save these threads in a vector
+	for (unsigned int i=0; i<coreNum; i++) {
+		// Create the unique string
+		char character = 97 + i;
+		string str = "";
+		for (int j=0; j<5; j++) {
+			str += character;
+		}
+		cout << "1st str:\t" << str << endl;
+		// Create the thread
+//		thread currentThread = thread(fun, str);
+//		// Add the thread
+//		vec.push_back(currentThread);
+
+		vec.push_back(thread(fun, "aaaaa"));
+	}
 
 	//TODO let threads run a bit (5 seconds)
 	this_thread::sleep_for(chrono::milliseconds(5000));
 	
 	//TODO ask them all to stop
+	doWork = false;
 	
 	//TODO wait for all threads to finish
+	for (auto& t: vec) t.join();
 	
 	return 0;
 }
